d3bf4564d2a68f8deae6bc5da2e2b746a9a09100
Shmuel Robinov <shmulik4rob@gmail.com>
2024-10-10 10:01:26 +0300

    Init project (#1)
    * initialize the vite project
    
    * initialize the vite project

---
+++ src/components/kibanaDashboard.tsx
@@ -0,0 +1,7 @@
+const KibanaDashboard = () => {
+  return <iframe
+    src="https://kolzchut-es-cluster.kb.westus2.azure.elastic-cloud.com:9243/app/dashboards#/view/3b4c5932-1f2e-4126-ae61-ffe7e87f4d77"
+    height="860" width="1950"></iframe>
+};
+
+export default KibanaDashboard;
