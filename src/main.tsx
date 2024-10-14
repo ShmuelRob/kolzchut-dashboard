815dce3e7ae6c437296c35dedb1b5e1fab8bffc2
Shmuel Robinov <shmuel_robinov@webiks.com>
2024-10-14 11:42:39 +0300

    create route

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,24 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import App from './App.tsx'
 import './index.css'
+import {createBrowserRouter, RouterProvider} from "react-router-dom";
+import Dashboard from "./pages/dashboard/dashboard.tsx";
+
+
+const router = createBrowserRouter([
+  // {
+  //   path: "/login",
+  //   element: <Login {...{setUser}}/>
+  // },
+  {
+    path: "/dashboard",
+    element: <Dashboard/>
+  },
+]);
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    {/*<App />*/}
+    <RouterProvider router={router}/>
   </StrictMode>,
 )
---
+++ src/pages/dashboard/dashboard.tsx
@@ -0,0 +1,11 @@
+import KibanaDashboard from "../../components/kibanaDashboard.tsx";
+import {generateSrc} from "./generateKey.ts";
+
+const Dashboard = () => {
+  const src = generateSrc()
+  return <>
+    <KibanaDashboard {...{src}} />
+  </>
+}
+
+export default Dashboard
