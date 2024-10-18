75f45eefd9e3c32072b1862617d1219a89bbb5f9
Shmuel Robinov <shmulik4rob@gmail.com>
2024-10-18 22:16:45 +0300

    add sso auth to the website (#6)
    * add sso auth to the website
    
    * Update AuthContext.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,24 +1,17 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
+import {StrictMode} from 'react'
+import {createRoot} from 'react-dom/client'
 import './index.css'
-import {createBrowserRouter, RouterProvider} from "react-router-dom";
-import Dashboard from "./pages/dashboard/dashboard.tsx";
-import Login from "./pages/login/Login.tsx";
-
-const router = createBrowserRouter([
-  {
-    path: "/login",
-    element: <Login/>
-  },
-  {
-    path: "/dashboard",
-    element: <Dashboard/>
-  },
-]);
+import {RouterProvider} from "react-router-dom";
+import {GoogleOAuthProvider} from "@react-oauth/google"
+import {AuthProvider} from "./auth/AuthContext.tsx";
+import {router} from "./router.tsx";
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    {/*<App />*/}
-    <RouterProvider router={router}/>
+    <GoogleOAuthProvider clientId={import.meta.env.VITE_GOOGLE_CLIENT_ID}>
+      <AuthProvider>
+        <RouterProvider router={router}/>
+      </AuthProvider>
+    </GoogleOAuthProvider>
   </StrictMode>,
 )
--- src/pages/login/Login.tsx
+++ src/pages/login/Login.tsx
@@ -1,8 +1,26 @@
+import {jwtDecode} from "jwt-decode";
+import {CredentialResponse, GoogleLogin} from "@react-oauth/google";
+import {useNavigate} from "react-router-dom";
+import {useAuth} from "../../auth/useAuth.ts";
+
 const Login = () => {
+  const navigate = useNavigate();
+  const {login} = useAuth();
+
+  const success = (credentialResponse: CredentialResponse) => {
+    const decoded = jwtDecode(credentialResponse.credential!);
+    login(decoded);
+    navigate("/dashboard");
+  }
+
+  const failed = () => console.log("Login Failed");
+
   return (
-    <p>
-      login page
-    </p>
+    <div>
+      <GoogleLogin
+        onSuccess={credentialResponse => success(credentialResponse)}
+        onError={failed}/>
+    </div>
   )
 }
 
