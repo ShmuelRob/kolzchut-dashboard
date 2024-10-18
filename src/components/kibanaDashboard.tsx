cf95f2f2a227bd8032dab793c0a72ec1f1625ac5
Shmuel Robinov <shmulik4rob@gmail.com>
2024-10-18 15:38:57 +0300

    Add routes (#3)
    * create route
    
    * create route

--- src/components/kibanaDashboard.tsx
+++ src/components/kibanaDashboard.tsx
@@ -1,7 +1,12 @@
-const KibanaDashboard = () => {
+interface Props {
+  src: string
+}
+
+const KibanaDashboard = ({src}: Props) => {
+
   return <iframe
-    src="https://kolzchut-es-cluster.kb.westus2.azure.elastic-cloud.com:9243/app/dashboards#/view/3b4c5932-1f2e-4126-ae61-ffe7e87f4d77"
-    height="670" width="1530"></iframe>
+    src={src}
+    height="903" width="1916"/>
 };
 
 export default KibanaDashboard;
